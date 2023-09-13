2fba26e9c9c01eb67f85a1f65aa04ee3c269d794
nkc20236190 <nkc23.uno.yudai@gmail.com>
2023-09-13 11:30:48 +0900

    9月13日更新

---
+++ Assets/Script/SceneButton.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneBotton : MonoBehaviour
+{
+    public string SceneName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(SceneName);
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
